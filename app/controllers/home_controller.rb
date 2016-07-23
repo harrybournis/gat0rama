f86af639d0ba243512632a7e81be997d3e86674d
harry bournis <harrybournis@gmail.com>
2016-07-23 16:50:37 +0300

    gitignored placeholder images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,12 @@ class HomeController < ApplicationController
 	def index
 		@yo = "yo"
 		@posts = Post.all
+
+		@images = []
+
+		17.times do |index|
+			@images << "placeholders/post" + (index + 1).to_s + ".jpeg"
+		end
+		logger.info @images
 	end
 end
