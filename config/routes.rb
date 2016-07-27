fce65b0a0444da2fdfdb4d83ce7b6d2a9c3b829d
harry bournis <harrybournis@gmail.com>
2016-07-27 17:24:24 +0300

    added container to posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 	root to: 'home#index'
 
 	get 'index', to: 'home#index'
+	get 'shop', to: 'home#shop'
+	get 'news', to: 'home#news'
 	get 'about', to: 'home#about'
 	get 'contribute', to: 'home#contribute'
 	get 'news', to: 'home#news'
