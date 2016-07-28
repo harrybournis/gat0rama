6b504624bae4633d34c7420f30a20a9045c4f353
harry bournis <harrybournis@gmail.com>
2016-07-28 13:41:27 +0300

    d

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
 		20.times do |index|
 			images << "placeholders/post%20(" + (index + 1).to_s + ").jpg"
 		end
-		Rails.logger.info images.first
+
 		return images
 	end
 end
