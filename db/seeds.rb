87cc40a12498913523b262a60105800066c3f228
harry bournis <harrybournis@gmail.com>
2016-10-02 15:01:17 +0300

    updated seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Setting.create
-Setting.first.update(value: {"header_logo"=>"/images/LOGOSITETEST.png", "navbar_links_color"=>"#063a35", "website_background_color"=>"transparent", "website_background_image"=>""}, description: "styling")
+s = Setting.first
+s.value = {"header_logo"=>"/images/LOGOSITETEST.png", "navbar_links_color"=>"#063a35", "website_background_color"=>"transparent", "website_background_image"=>""}
+s.save
 ParalaxImage.create(url: "/placeholders/clown.gif")
 ParalaxImage.create(url: "/placeholders/adidas.png")
 Randomizer.post(30)
