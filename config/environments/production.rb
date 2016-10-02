37e2bf40b2066f3a39df2487636a1076e19c44ee
harry bournis <harrybournis@gmail.com>
2016-10-02 15:10:10 +0300

    removed manually loading secret

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,11 +83,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # if ENV["SECRET_KEY_BASE"]
-  #   puts 'yes secret key base'
-  #   config.secret_key_base = ENV["SECRET_KEY_BASE"]
-  # else
-  #   puts "no secret key base"
-  # end
 end
