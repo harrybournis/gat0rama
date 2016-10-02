e044b46049383301d81179e59b0ec3d74b2ec5a2
harry bournis <harrybournis@gmail.com>
2016-10-02 14:27:03 +0300

    what

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  puts "what"
   if EVN["SECRET_KEY_BASE"]
     puts 'yes secret key base'
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
